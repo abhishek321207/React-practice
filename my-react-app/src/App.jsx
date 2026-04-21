227970837cf86a8ca6786c4f556f3dd237cc457a
Abhishek Deep <deepabhishek25@gmail.com>
2026-04-21 12:53:55 +0530

    created a simple button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,17 @@
-import Card from './card.jsx'
-
+import React from "react";
+import Button from "./Button/Button.jsx";
+import Card from "./Card.jsx";
+import Food from "./Food.jsx";
+import Footer from "./Footer.jsx";
 function App() {
-  return(
-    <Card/>
+  return (
+    <React.Fragment>
+      <Card />
+      <Food/>
+      <Button />
+      <Footer/>
+    </React.Fragment>
   );
-    
-
 }
 
-export default App
+export default App;
