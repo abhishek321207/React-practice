7d30145cf15cb57f45520fd3806025eb348d9821
Abhishek Deep <deepabhishek25@gmail.com>
2026-04-21 15:47:18 +0530

    created a student list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button.jsx";
+import Student from './Student/Student.jsx';
 import Card from "./Card.jsx";
 import Food from "./Food.jsx";
 import Footer from "./Footer.jsx";
@@ -9,6 +10,10 @@ function App() {
       <Card />
       <Food/>
       <Button />
+      <Student name="Abhishek" age={23} isStudent={true} />
+      <Student name="Abhigyan" age={42} isStudent={false}/>
+      <Student name="Debo" age={50} isStudent={false}/>
+      <Student name="Yash" age={27} isStudent={true}/>
       <Footer/>
     </React.Fragment>
   );
