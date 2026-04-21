fe01d962c0dedd767d1094f65ce730e9a6c95158
Abhishek Deep <deepabhishek25@gmail.com>
2026-04-21 11:00:17 +0530

    did some changes

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+
+function Card(){
+    return(
+        <div className="card">
+              <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
+              <h2 className="card-title"> Bro Code</h2>  
+              <p className="card-text">I make food and play games</p>         
+
+
+        </div>
+
+    );
+}
+
+export default Card
